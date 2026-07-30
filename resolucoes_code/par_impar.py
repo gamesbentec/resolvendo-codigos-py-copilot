6fa7b7560a2807fd47f4ae058b7ca843cb25303b
gamesbentec <gamesbentec@gmail.com>
2026-07-30 13:54:15 -0300

    Verificando se um número é par ou ímpar
    Verificando se um número é par ou ímpar.

---
+++ resolucoes_code/par_impar.py
@@ -0,0 +1,12 @@
+# Verificando se um número é par ou ímpar
+
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 2 == 0:
+    print(f"{numero} é um número par.")
+else:
+  print(f"{numero} é um número ímpar.")
+
+# Exemplo
+Digite um número inteiro: 15
+15 é um número ímpar.
